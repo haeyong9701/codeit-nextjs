b486d30c617510db38985c55d99d53d902335746
haeyong9701 <haeyong9701@gmail.com>
2024-02-11 01:11:18 +0900

    feat: api 연동

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,30 @@
+import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
 import { useRouter } from "next/router";
+import axios from "@/lib/axios";
+import { useEffect, useState } from "react";
 
 const Search = () => {
+  const [products, setProducts] = useState([]);
   const router = useRouter();
   const { q } = router.query;
+
+  async function getProducts(query) {
+    const res = await axios.get(`/products/?q=${query}`);
+    const nextProducts = res.data.results;
+    setProducts(nextProducts);
+  }
+
+  useEffect(() => {
+    getProducts(q);
+  }, [q]);
 
   return (
     <div>
       <h1>Search 페이지</h1>
       <SearchForm initialValue={q} />
       <h2>{q} 검색 결과</h2>
+      <ProductList products={products} />
     </div>
   );
 };
