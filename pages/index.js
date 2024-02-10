86985f6c3c237a2586a18c755307a68393a84679
haeyong9701 <haeyong9701@gmail.com>
2024-02-10 23:33:45 +0900

    feat: Link 컴포넌트

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import Link from "next/link";
 import styles from "@/styles/Home.module.css";
 
-const Home = () => {
-  return <h1 className={styles.title}>안녕 Next.js</h1>;
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <div>
+      <h1>Codeitmall</h1>
+      <ul>
+        <li>
+          <Link href="/products/1">첫 번째 상품</Link>
+        </li>
+        <li>
+          <Link href="/products/2">두 번째 상품</Link>
+        </li>
+        <li>
+          <Link href="/products/3">세 번째 상품</Link>
+        </li>
+        <li>
+          <Link href="https://codeit.kr">코드잇</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
