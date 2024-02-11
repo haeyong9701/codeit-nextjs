5ea6604a8fe316df51a2972b73fe21bdfdadb371
haeyong9701 <haeyong9701@gmail.com>
2024-02-11 11:50:48 +0900

    feat: Context 활용

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import Header from "@/components/Header";
+import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/Home.module.css";
+import "@/styles/global.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />;
+      <ThemeProvider>
+        <Header />
+        <Component {...pageProps} />;
+      </ThemeProvider>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import SearchForm from "@/components/SearchForm";
 import ProductList from "@/components/ProductList";
 import axios from "@/lib/axios";
 import { useEffect, useState } from "react";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
@@ -18,7 +19,7 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Codeitmall</h1>
+      <h1 className={styles.title}>Codeitmall</h1>
       <SearchForm />
       <ProductList products={products} />
     </div>
