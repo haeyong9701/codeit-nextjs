3ffc0b93b4638a6aa7e120f2146d8971bb0989fe
haeyong9701 <haeyong9701@gmail.com>
2024-02-11 20:39:06 +0900

    feat: 구글 폰트 적용하기

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,12 @@ import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/Home.module.css";
 import "@/styles/global.css";
 import Head from "next/head";
+import { Noto_Sans_KR } from "next/font/google";
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ["400", "700"],
+  subsets: [],
+});
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,10 +17,12 @@ export default function App({ Component, pageProps }) {
         <title>Codeitmall</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ThemeProvider>
-        <Header />
-        <Component {...pageProps} />;
-      </ThemeProvider>
+      <main className={notoSansKR.className}>
+        <ThemeProvider>
+          <Header />
+          <Component {...pageProps} />;
+        </ThemeProvider>
+      </main>
     </>
   );
 }
